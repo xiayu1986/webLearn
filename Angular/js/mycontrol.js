bd62c7fa79c2b76b733a0b032a2c32ed702fc5ba
xiayu_lt@126.com <xiayu_lt@126.com>
2016-09-24 08:02:05 +0800

    修改新增加的angular.js项目

--- Angular/js/mycontrol.js
+++ Angular/js/mycontrol.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2015/5/6.
  */
-
+var app=angular.module("app",[]);
 app.controller("myTodoCtrl", function($scope) {
     $scope.message = "";
     $scope.left  = function() {return 100 - $scope.message.length;};

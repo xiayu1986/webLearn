23ac95e0d1794f1ba25bea137d05449033953b24
xia_yu <xia_yu@Ctrip.com>
2016-11-24 12:43:27 +0800

    调整jquery插件语法

--- webpack-vue/src/App.vue
+++ webpack-vue/src/App.vue
@@ -20,12 +20,12 @@
           column: 'name',
           order: 1
         },
-        gridColumns: [{"name":"出发城市","key":"city"},{"name":"飞行时长","key":"time"}],
+        gridColumns: [{"name":"出发城市","key":"city"},{"name":"飞行时长","key":"time"},{"name":"起飞时间","key":"depTime"}],
         gridData: [
-        { city: '北京', time:"2h30m" },
-        { city: '上海', time:"1h3m" },
-        { city: '重庆', time:"2h10m" },
-        { city: '南京', time:"2h40m" }
+        { city: '北京', time:"2h30m" ,depTime:"2016-11-24 13:00:00"},
+        { city: '上海', time:"1h3m" ,depTime:"2016-11-26 17:12:00"},
+        { city: '重庆', time:"2h10m" ,depTime:"2016-11-23 09:30:00"},
+        { city: '南京', time:"2h40m" ,depTime:"2016-11-27 21:30:00"}
         ]
       }
     }, components: {

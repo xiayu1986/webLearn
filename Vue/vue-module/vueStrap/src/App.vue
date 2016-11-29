256dc3debae101085e2a6c2851646e2716ec079d
xia_yu <xia_yu@Ctrip.com>
2016-11-29 16:19:00 +0800

    添加vueStrap项目

---
+++ Vue/vue-module/vueStrap/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+import {modal} from 'vue-strap';
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+

a4f068e61ad4ac321c872b936344cc226fcd5318
xia_yu <xia_yu@Ctrip.com>
2017-05-17 09:18:28 +0800

    修改接口文件

--- OOP/js/interface.js
+++ OOP/js/interface.js
@@ -19,4 +19,16 @@ Interface.ensureImplements=function(object){
     if(arguments.length<2){
         throw new Error("该方法需要传入两个参数");
     }
+    for(var i=1,len=arguments.length;i<len;i++){
+        var interface=arguments[i];
+        if(interface.constructor!==Interface){
+            throw new Error("未实现原型中的方法")
+        }
+        for(var j=0,methodsLen=interface.methods.length;j<methodsLen;j++){
+            var method=interface.methods[j];
+            if(!object[method] || typeof object[method]!=='function'){
+                throw new Error("function Interface.ensureImplements:object does not implenent the"+interface.name+"interface.Method"+method+"was not found");
+            }
+        }
+    }
 }

1fe738348f779a3b6e6122e335628dc480ece4b1
xia_yu <xia_yu@Ctrip.com>
2017-08-09 09:40:23 +0800

    调整header样式

--- Vue/2.0/vue-app/src/components/header/header.vue
+++ Vue/2.0/vue-app/src/components/header/header.vue
@@ -24,7 +24,7 @@
   </div>
 </template>
 
-<style lang="stylus" rel="stylesheet/stylus">
+<style rel="stylesheet/stylus" type="text/stylus">
   .header
     color: rgb(255, 255, 255)
     background:rgb(0,0,0)
@@ -33,10 +33,49 @@
       font-size:0
       .avatar
         display:inline-block
+        vertical-align :top
+        img
+          border-radius :4px
       .content
         display: inline-block
         font-size:14px
-
+        margin-left :16px
+        .title
+          font-size:0
+          line-height 18px
+          font-weight :bold
+          .brand
+            display: inline-block
+            vertical-align :top
+            width :30px
+            height:18px
+            background-image:url('brand@2x.png')
+            background-size:30px 18px
+            background-repeat:no-repeat
+            margin-right :12px
+          .name
+            font-size:16px
+        .description
+          font-size:12px
+          line-height: 12px
+          font-weight :200
+          margin:6px 0 8px
+        .supports
+          font-size: 0
+          font-weight :200
+          .icon
+            display: inline-block
+            width:12px
+            height:12px
+            background-image :url('decrease_1@2x.png')
+            background-size:12px 12px
+            background-repeat :no-repeat
+            margin-right :8px;
+            vertical-align:top
+          .text
+            font-size:10px
+            line-height :12px
+            display:inline-block
 </style>
 
 <script type="es6">
